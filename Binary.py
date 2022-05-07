1b20d9998e5a6b28620804d79e2976e9e7a3c660
Andrew Lubanyana <100496410+Andrewlubanyana@users.noreply.github.com>
2022-05-07 15:49:15 +0200

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -11,4 +11,12 @@ def binary_algo(list, target):
   else:
     last = midpoint - 1
     
-    
+    def verify(index):
+ if index is not None:
+    print ("Target found at index: ", index)
+ else: 
+    print("Target not found in list")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+results = binary_algo (numbers, 12)
+verify(results)
