bc9772c6445c5184901967811c2a76c95cda08a7
Andrew Lubanyana <100496410+Andrewlubanyana@users.noreply.github.com>
2022-05-07 15:40:52 +0200

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,14 @@
+def binary_algo(list, target):
+  first = 0
+  last = len(list) - 1
+  
+  while first <= last
+    midpoint = len(first + last) // 2
+  if [midpoint] == target:
+    return midpoint
+  elif list [midpoint] < target:
+    first = midpoint + 1
+  else:
+    last = midpoint - 1
+    
+    
